12e845261ce0c432197a687a7270185a86ab67f5
kotamanegi <kotamanegi84@gmail.com>
2020-11-27 16:27:43 +0900

    add some Interface

--- src/myrobot/SearchRobot.java
+++ src/myrobot/SearchRobot.java
@@ -1,5 +1,63 @@
 package myrobot;
+import java.io.IOException;
 
-public class SearchRobot {
+import robocode.*;
+//import java.awt.Color;
 
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * EclipseRobot - a robot by (your name here)
+ */
+public class SearchRobot extends TeamRobot
+{
+	/**
+	 * run: EclipseRobot's default behavior
+	 */
+	public void run() {
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			//setAhead(100);
+			setTurnRadarLeft(100);
+			execute();
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		//fire(1);
+       String[] teammates = getTeammates();
+       if (teammates != null) {
+           for (String member : teammates) {
+               if(member.startsWith(e.getName())) return;
+           }
+       }
+		System.out.println(e.getName());
+		RobotInfo info = new RobotInfo();
+		info.setValue(this, e.getBearingRadians(), e.getDistance(), e.getHeadingRadians());
+		try {
+			broadcastMessage(info);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
 }
