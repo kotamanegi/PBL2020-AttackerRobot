01c64327e35559d97b06ddc37dc1474a1fe40a1e
grtw2116 <taru0116@gmail.com>
2020-12-04 16:03:45 +0900

    remove conflict

--- src/myrobot/SearchRobot.java
+++ src/myrobot/SearchRobot.java
@@ -1,42 +1,12 @@
 package myrobot;
+
 import java.io.IOException;
 
-<<<<<<< HEAD
-import java.io.IOException;
-=======
 import robocode.*;
 //import java.awt.Color;
->>>>>>> refs/remotes/upstream/master
 
-<<<<<<< HEAD
-import robocode.*;
-//import java.awt.Color;
-=======
 // API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
->>>>>>> refs/remotes/upstream/master
 
-<<<<<<< HEAD
-// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
-=======
-/**
- * EclipseRobot - a robot by (your name here)
- */
-public class SearchRobot extends TeamRobot
-{
-	/**
-	 * run: EclipseRobot's default behavior
-	 */
-	public void run() {
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			//setAhead(100);
-			setTurnRadarLeft(100);
-			execute();
-		}
-	}
->>>>>>> refs/remotes/upstream/master
-
-<<<<<<< HEAD
 /**
  * EclipseRobot - a robot by (your name here)
  */
@@ -50,33 +20,10 @@ public class SearchRobot extends TeamRobot {
 			// setAhead(100);
 			setTurnRadarLeft(100);
 			execute();
-=======
-	/**
-	 * onScannedRobot: What to do when you see another robot
-	 */
-	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
-		//fire(1);
-       String[] teammates = getTeammates();
-       if (teammates != null) {
-           for (String member : teammates) {
-               if(member.startsWith(e.getName())) return;
-           }
-       }
-		System.out.println(e.getName());
-		RobotInfo info = new RobotInfo();
-		info.setValue(this, e.getBearingRadians(), e.getDistance(), e.getHeadingRadians());
-		try {
-			broadcastMessage(info);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
->>>>>>> refs/remotes/upstream/master
 		}
 	}
 
 	/**
-<<<<<<< HEAD
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
@@ -141,19 +88,4 @@ public class SearchRobot extends TeamRobot {
 		// Replace the next line with any behavior you would like
 		back(20);
 	}
-=======
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		back(10);
-	}
-	
-	/**
-	 * onHitWall: What to do when you hit a wall
-	 */
-	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
-		back(20);
-	}	
->>>>>>> refs/remotes/upstream/master
 }
